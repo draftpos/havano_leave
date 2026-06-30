201ea3e0724f96769a9220a74fd3ec5700cc2fd8
makonia20 <makonia20@gmail.com>
2026-06-30 12:30:03 +0200

    feat: add Havano Leave Ledger Entry

---
+++ havano_leave/havano_leave/doctype/havano_leave_ledger_entry/havano_leave_ledger_entry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, chirovemunyaradzi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HavanoLeaveLedgerEntry(Document):
+	pass
